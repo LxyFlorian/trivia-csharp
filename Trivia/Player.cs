da19772bb0339a6986b2bf3c843cde37dc0fdab9
Yoann Marguerite <yoann.marguerite@orange.fr>
2021-03-24 14:44:00 +0100

    class player ajouter

---
+++ Trivia/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trivia
+{
+    class Player
+    {
+        public string nom;
+        public int purse;
+        public int place;
+        public bool joker;
+        public bool penalty;
+
+        public Player (string nom, int place)
+        {
+            this.nom = nom;
+            this.purse = 0;
+            this.place = place;
+            this.joker = false;
+            this.penalty = false;
+        }
+    }
+}
